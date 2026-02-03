137d0df5366a1ea27420ceb590e4e0fd558f9300
Nithya-Ganesan87 <nithyaganesan87@gmail.com>
2026-02-03 17:44:24 +0530

    Add files via upload

---
+++ basicFile.js
@@ -0,0 +1,12 @@
+let firstName = "Nithya"
+let companyName = "TestLeaf"
+let mobileNumber= 8967879999n
+let isAutomation = true
+let hasPlaywright
+let javaknowledge=null
+console.log("Emp firstname ",firstName+" with type "+typeof(firstName))
+console.log("Company name is ",companyName+" with type "+typeof(companyName))
+console.log("Mobile number ",mobileNumber+" with type "+typeof(mobileNumber))
+console.log("Automation status ",isAutomation+" with type "+typeof(isAutomation))
+console.log("Uses Playwright  "+hasPlaywright+" with type "+typeof(hasPlaywright))
+console.log("Has Java knowledge "+javaknowledge+" with type "+typeof(javaknowledge))
